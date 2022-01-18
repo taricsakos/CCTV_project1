315e959cfedbb6b60511268e5ee13201ed084be9
kovacsbalint1 <60219560+kovacsbalint1@users.noreply.github.com>
2022-01-18 16:56:24 +0100

    Update facerec_pi_test_profiles.py

--- python_code/facerec_pi_test_profiles.py
+++ python_code/facerec_pi_test_profiles.py
@@ -1,69 +1,42 @@
-#Modified by smartbuilds.io
-#Date: 03.07.20
-#Desc: This script is running a face recongition of a live webcam stream. This is a modifed
-#code of the orginal Ageitgey (GitHub) face recognition demo to include multiple faces.
-#Simply add the your desired 'passport-style' face to the 'profiles' folder.
-
 import face_recognition
 import cv2
 import numpy as np
 import os
 
-# Note: This script requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# Visit smartbuids.io for more information
-
-# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-#Store objects in array
-known_person=[] #Name of person string
-known_image=[] #Image object
-known_face_encodings=[] #Encoding object
+known_person=[]
+known_image=[]
+known_face_encodings=[]
 
-# Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
 process_this_frame = True
 
-#Loop to add images in friends folder
 for file in os.listdir("profiles"):
     try:
-        #Extracting person name from the image filename eg: david.jpg
         known_person.append(file.replace(".jpg", ""))
         file=os.path.join("profiles/", file)
         known_image = face_recognition.load_image_file(file)
-        #print(face_recognition.face_encodings(known_image)[0])
         known_face_encodings.append(face_recognition.face_encodings(known_image)[0])
-        #print(known_face_encodings)
 
     except Exception as e:
         pass
     
-#print(len(known_face_encodings))
-#print(known_person)
-
-
 while True:
-    # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Only process every other frame of video to save time
     if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         global name_gui;
-        #face_names = []
         for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
             
