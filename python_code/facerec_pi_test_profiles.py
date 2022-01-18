318d9eff547926c658bb0f1e7d0c4d8da2fc1724
kovacsbalint1 <60219560+kovacsbalint1@users.noreply.github.com>
2022-01-18 16:57:17 +0100

    Update facerec_pi_test_profiles.py

--- python_code/facerec_pi_test_profiles.py
+++ python_code/facerec_pi_test_profiles.py
@@ -39,47 +39,34 @@ while True:
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
-            
-            #print(face_encoding)
-            #print(matches)
 
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_person[best_match_index]
 
-            #print(name)
-            #print(face_locations)
             face_names.append(name)
     
             name_gui = name
 
     process_this_frame = not process_this_frame
 
-
-    # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
         right *= 4
         bottom *= 4
         left *= 4
 
-        # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (255, 255, 255), 2)
 
-        # Draw a label with a name below the face
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (255, 255, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name_gui, (left + 10, bottom - 10), font, 1.0, (0, 0, 0), 1)
 
-    # Display the resulting image
     cv2.imshow('Video', frame)
 
-    # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
